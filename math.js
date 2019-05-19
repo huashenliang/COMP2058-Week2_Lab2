554a7dad90050cff09e3ffc59408343ffb4ce8f9
huashen liang <huashenliang@MacBook-Air-de-huashen.local>
2019-05-19 10:40:57 -0400

    Validation for numbers and method

--- math.js
+++ math.js
@@ -2,7 +2,7 @@ const handleCalculation = (method, x, y) => {
     switch(method.toLowerCase()){
         case 'add':
             return result = x + y;
-        case 'substract':
+        case 'subtract':
             return result = x - y;
         case 'multiply':
             return result = x * y;
@@ -17,7 +17,7 @@ const handleMethodSign = (method) => {
     switch(method){
         case 'add':
             return sign = '+';
-        case 'substract':
+        case 'subtract':
             return sign = '-';
         case 'multiply':
             return sign = '*';
@@ -28,6 +28,24 @@ const handleMethodSign = (method) => {
     }
 }
 
+const validMethod = ['add', 'subtract', 'divide', 'multiply'];
+
+const handleNumberValidation = (x, y, res) => {
+    //checking if x and y are numbers
+    if(isNaN(x) || isNaN(y)){
+        return res.send(`Both x: ${x} and y: ${y} must be a number`);
+    } 
+}
+
+const handleMethodValidation = (method, res) => {
+
+    //checking if the method entered is valid
+   if(!validMethod.includes(method.toLowerCase())){
+       return res.send(`${method} is not a valid method. It must be one of the following:
+        ${validMethod.toString()}
+       `);
+   }
+}
 
 const handleHttpCalculation = (req, res) => {
     const query = req.query;
@@ -35,6 +53,9 @@ const handleHttpCalculation = (req, res) => {
     const x = Number(query.x);
     const y = Number(query.y);
     const method = query.method;
+    
+    handleNumberValidation(x,y,res);
+    handleMethodValidation(method,res);
 
     const result = handleCalculation(method, x, y);
     const sign = handleMethodSign(method);
