ffbd12febbbbe81519d3ccd6f47c9fab8fcf47af
huashen liang <huashenliang@MacBook-Air-de-huashen.local>
2019-05-19 09:49:16 -0400

    Finalizing server.js - import math

---
+++ math.js
@@ -0,0 +1,5 @@
+const handleHttpCalculation = (req, res) => {
+    res.send(`Hello World`);
+};
+
+module.exports = handleHttpCalculation; //Export the calculation function
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const doMath = require('./math'); //import math
 
+
+//At the index of our application - use the handle calucation function from 
+//math.js file to handle the request
+app.get('/', doMath);
 app.listen(PORT);
-console.log(`Your application is running at http://localhose:${PORT}`);
+console.log(`Your application is running at http://localhost:${PORT}`);
